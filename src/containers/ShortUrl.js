8432c0bda877c5f293c525542be2c71b782cbdd5
Baktiyar Ishenov <b1963826@gmail.com>
2020-02-28 17:27:36 +0600

    finish

---
+++ src/containers/ShortUrl.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import {Button, Form, FormGroup, Input} from "reactstrap";
+import {connect} from "react-redux";
+import {createShortLink} from "../store/actions";
+
+class ShortUrl extends Component {
+	state = {
+		originalUrl: ''
+	};
+
+	inputChangeHandler = event => {
+		this.setState({[event.target.name]: event.target.value});
+	};
+
+	submitFormHandler = event => {
+		event.preventDefault();
+		this.props.createShortLink(this.state);
+		this.setState({originalUrl: ''});
+	};
+
+	render() {
+		const shortLink = 'http://localhost:8000/' + this.props.shortUrl;
+
+		let shortLinkStyle = {
+			display: 'none'
+		};
+
+		if (this.props.shortUrl) {
+			shortLinkStyle = {
+				display: 'block'
+			};
+		}
+
+		return (
+			<>
+				<Form onSubmit={this.submitFormHandler}>
+					<FormGroup className="text-center m-4">
+						<h1 className="m-4">Shorten your link!</h1>
+						<Input
+							required
+							type="text"
+							name="originalUrl"
+							id="originalUrl"
+							placeholder="Enter URL here"
+							onChange={this.inputChangeHandler}
+							value={this.state.originalUrl}
+						/>
+						<Button className="m-4">Shorten!</Button>
+					</FormGroup>
+				</Form>
+				<div style={shortLinkStyle}>
+					<h3>Your link now looks like this:</h3>
+					<a href={shortLink}>{shortLink}</a>
+				</div>
+			</>
+		);
+	}
+}
+
+const mapStateToProps = state => ({
+	shortUrl: state.shortUrl
+});
+
+const mapDispatchToProps = dispatch => ({
+	createShortLink: (url) => dispatch(createShortLink(url))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShortUrl);
